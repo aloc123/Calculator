d517534aee34024ee4543fd0c6a1e5bafa282772
Alec_Zam <47293857+aloc123@users.noreply.github.com>
2025-11-07 17:04:26 +0800

    Append to screen the button pressed

--- script.js
+++ script.js
@@ -26,11 +26,22 @@ for (let i = 0; i < 5; i++){
     const row = document.createElement("div");
     row.style.cssText = "display: flex; flex-direction: row; justify-content: space-between; border-style: none; border-color: black; flex: 1;";
     row.classList.add("row-buttons");  
+    //add buttons with proper label
     for (let index = 0; index < 4; index++){
         const button = document.createElement("button");
         button.style.cssText = "flex: 1; border-style: solid; border-radius: 4px; margin: 5px;"
         button.textContent = buttonMap[i.toString() + index.toString()];
+        button.classList.add(`${i.toString() + index.toString()}`);
         row.appendChild(button);
     }
     buttons.appendChild(row);
 }
+
+const screen = document.querySelector(".screen-container");
+const buttonsList = document.querySelectorAll("button");
+buttonsList.forEach((btn) => {
+    //add eventlistener, when clicked, gets the class name then appends the corresponding key to the screen
+    btn.addEventListener("click", (event) => {
+        screen.textContent += buttonMap[btn.className];
+    });
+});
