f4ed8043c103221684136265d9b43ba6b22798ef
Alec_Zam <47293857+aloc123@users.noreply.github.com>
2025-11-07 16:48:00 +0800

    Add formatted html elements

---
+++ script.js
@@ -0,0 +1,36 @@
+const buttons = document.querySelector(".button-container");
+const buttonMap = {
+    "00": "CLR",
+    "01": "DEL",
+    "02": "%",
+    "03": "/",
+    "10": "1",
+    "11": "2",
+    "12": "3",
+    "13": "X",
+    "20": "4",
+    "21": "5",
+    "22": "6",
+    "23": "-",
+    "30": "7",
+    "31": "8",
+    "32": "9",
+    "33": "+",
+    "40": "0",
+    "41": ".",
+    "42": "+/-",
+    "43": "=",
+};
+//create 3 row
+for (let i = 0; i < 5; i++){
+    const row = document.createElement("div");
+    row.style.cssText = "display: flex; flex-direction: row; justify-content: space-between; border-style: none; border-color: black; flex: 1;";
+    row.classList.add("row-buttons");  
+    for (let index = 0; index < 4; index++){
+        const button = document.createElement("button");
+        button.style.cssText = "flex: 1; border-style: solid; border-radius: 4px; margin: 5px;"
+        button.textContent = buttonMap[i.toString() + index.toString()];
+        row.appendChild(button);
+    }
+    buttons.appendChild(row);
+}
